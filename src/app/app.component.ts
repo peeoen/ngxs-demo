e223d6b09ae222364b308ffc58a17913d0604bde
peeoen <theerawat.pon@gmail.com>
2018-05-31 14:22:57 +0700

    add example action-handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
+import { Actions, Select, Store, ofActionSuccessful } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { withLatestFrom } from 'rxjs/operators';
 import { AddFood, FoodsState } from './food/food.state';
@@ -11,13 +11,18 @@ import { AddFood, FoodsState } from './food/food.state';
 })
 export class AppComponent {
   @Select(FoodsState.foods) foods$: Observable<any>;
-  constructor(private store: Store) {
+  constructor(private store: Store,
+  private action$: Actions) {
     this.store.dispatch(new AddFood('Fire Rice'));
+    this.delayAddFood();
+    this.action$.pipe(ofActionSuccessful(AddFood)).subscribe(() => {
+      console.log('add food');
+    });
+  }
 
+  private delayAddFood() {
     setTimeout(() => {
-      this.store.dispatch(new AddFood('Omelette')).pipe(
-        withLatestFrom(this.foods$)
-      ).subscribe(([foods]) => {
+      this.store.dispatch(new AddFood('Omelette')).pipe(withLatestFrom(this.foods$)).subscribe(([foods]) => {
         // console.log(foods);
       });
     }, 3000);
