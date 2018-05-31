31be43fa264df05f46df1f00b4d156b02fcafb69
peeoen <theerawat.pon@gmail.com>
2018-05-31 16:28:39 +0700

    reinitial tempalte ngxs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
-import { Login, Logout } from './auth/auth.state';
+
 
 
 @Component({
@@ -10,23 +9,10 @@ import { Login, Logout } from './auth/auth.state';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
-  username: string;
-  password: string;
-  user$: Observable<any>;
+
 
   constructor(private store: Store) {
-    this.user$ = this.store.select(state => state);
+ 
   }
 
-  login() {
-    this.store.dispatch(new Login({
-      username: this.username,
-      password: this.password
-    }));
-  }
-
-  logout() {
-    this.store.dispatch(new Logout());
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FoodsState } from './food/food.state';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,8 +6,6 @@ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { AppComponent } from './app.component';
-import { AuthService } from './auth/auth.service';
-import { AuthState } from './auth/auth.state';
 
 @NgModule({
   declarations: [
@@ -16,15 +15,12 @@ import { AuthState } from './auth/auth.state';
     BrowserModule,
     FormsModule,
     NgxsModule.forRoot([
-      AuthState
+      FoodsState
     ]),
-    NgxsStoragePluginModule.forRoot({
-      key: 'auth.token'
-    }),
     NgxsReduxDevtoolsPluginModule.forRoot()
   ],
   providers: [
-    AuthService
+
   ],
   bootstrap: [AppComponent]
 })
